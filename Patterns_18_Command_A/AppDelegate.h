5f04275e757aea3736076ffc11154aedf7486482
Ильяс <i.almakaev@gmail.com>
2017-08-22 14:45:50 +0700

    Initial Commit

---
+++ Patterns_18_Command_A/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Patterns_18_Command_A
+//
+//  Created by Ильяс on 22.08.17.
+//  Copyright © 2017 Алмакаев Ильяс. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
